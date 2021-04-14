c403b943b6e37f5a9b02dba0b4ee4a8c21b5c522
FMTriguero <FMTriguero@gmail.com>
2021-04-14 10:08:37 +0800

    Moves camera to its own file

---
+++ Camera/camera.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+import mss
+import numpy as np
+
+
+class Camera:
+    def __init__(self):
+        self.bounds = []
+        self.sct = mss.mss()
+        self.monitor = self.sct.monitors[1]
+
+    def take_screenshot(self):
+        screenshot = self.sct.grab(self.monitor)
+        return screenshot
+
+    def transform_screenshot_to_np(self, screenshot):
+        screenshot = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+        image = screenshot.resize((screenshot.size[0], screenshot.size[1]))
+        np_image = np.asarray(image.convert("RGB"))
+        return np_image
+
+    def get_numpy_screenshot(self):
+        screenshot = self.take_screenshot()
+        np_screenshot = self.transform_screenshot_to_np(screenshot)
+        return np_screenshot
--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 from ui.ui import *
+from Camera.camera import Camera
 
 from threading import Thread
-from PIL import Image
 
-import mss
 import keyboard
-import numpy as np
+
 import time
-
-
-class Camera:
-    def __init__(self):
-        self.bounds = []
-        self.sct = mss.mss()
-        self.monitor = self.sct.monitors[1]
-
-    def take_screenshot(self):
-        screenshot = self.sct.grab(self.monitor)
-        return screenshot
-
-    def transform_screenshot_to_np(self, screenshot):
-        screenshot = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
-        image = screenshot.resize((screenshot.size[0], screenshot.size[1]))
-        np_image = np.asarray(image.convert("RGB"))
-        return np_image
-
-    def get_numpy_screenshot(self):
-        screenshot = self.take_screenshot()
-        np_screenshot = self.transform_screenshot_to_np(screenshot)
-        return np_screenshot
 
 
 class Application:
